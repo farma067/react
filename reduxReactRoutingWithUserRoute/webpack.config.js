0497194635dcd85448bb16fda9547c6ec62bb04c
farma067 <farma067@yahoo.com>
2019-05-04 10:25:21 +0200

    webpack config changed

--- reduxReactRoutingWithUserRoute/webpack.config.js
+++ reduxReactRoutingWithUserRoute/webpack.config.js
@@ -3,12 +3,16 @@ module.exports = {
     './src/index.js'
   ],
   output: {
-    filename: './bundle.js'
+    filename: './bundle.js',
+    publicPath: '/'
   },
   module: {
     loaders: [{
       exclude: /node_modules/,
       loader: 'babel'
     }]
+  },
+  devServer: {
+    historyApiFallback: true,
   }
 };
